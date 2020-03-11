234a89d432f95c06a47681d0956fe02a213eed28
Touhidul Islam <russel2005@gmail.com>
2020-03-11 13:12:10 -0400

    Create CrewUserInfo.java

---
+++ src/test/java/com/xyz/retail/data/support/CrewUserInfo.java
@@ -0,0 +1,24 @@
+package com.xyz.retail.data.support;
+
+public class CrewUserInfo {
+
+	private String userId;
+
+	private String crewType;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getCrewType() {
+		return crewType;
+	}
+
+	public void setCrewType(String crewType) {
+		this.crewType = crewType;
+	}
+}
