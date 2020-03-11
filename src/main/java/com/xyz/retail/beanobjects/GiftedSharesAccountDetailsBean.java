6002135ae40e801612869c577766f509ad7a7fb1
Touhidul Islam <russel2005@gmail.com>
2020-03-11 10:19:38 -0400

    Create GiftedSharesAccountDetailsBean.java

---
+++ src/main/java/com/xyz/retail/beanobjects/GiftedSharesAccountDetailsBean.java
@@ -0,0 +1,74 @@
+package com.xyz.retail.beanobjects;
+
+public class GiftedSharesAccountDetailsBean {
+	public String accountName;
+
+	public String fundSymbol;
+
+	public String costbasisMethod;
+
+	public float quantity;
+
+	public String totalCost;
+
+	public String giftValue;
+
+	public String recipient;
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getFundSymbol() {
+		return fundSymbol;
+	}
+
+	public void setFundSymbol(String fundSymbol) {
+		this.fundSymbol = fundSymbol;
+	}
+
+	public String getCostbasisMethod() {
+		return costbasisMethod;
+	}
+
+	public void setCostbasisMethod(String costbasisMethod) {
+		this.costbasisMethod = costbasisMethod;
+	}
+
+	public float getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(float quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(String totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public String getGiftValue() {
+		return giftValue;
+	}
+
+	public void setGiftValue(String giftValue) {
+		this.giftValue = giftValue;
+	}
+
+	public String getRecipient() {
+		return recipient;
+	}
+
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+
+}
